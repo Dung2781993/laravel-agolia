41ad041a3bc4641811dbca19db6f18bc9c5cd645
JasonLe <Dung2781993@gmail.com>
2022-09-03 02:47:46 +1000

    Update agolia search

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,6 +10,30 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $input = $request->all();
+        $filterParams = $request->all();
+        $query = null;
+
+        foreach ($filterParams as $id => $value) {
+            if ($query) {
+                $query = $id . ':' . $value . ' AND ' . $query;
+            } else {
+                $query = $id . ':' . $value;
+            }
+        }
+
+        $result = User::search($query)->get();
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Get User successfully',
+                'data' => $result,
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => $result
+        ];
     }
 }
